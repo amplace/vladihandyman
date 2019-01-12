89ed5c91044e8ab29dd4c775bec6b2ca1f800fc1
Annie <Anniebelkin97@gmail.com>
2019-01-12 15:10:51 +0200

    Add codebase

---
+++ gulp/tasks/build.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cssnano = require('gulp-cssnano'),
+    browserSync = require('browser-sync').create(),
+    del = require('del');
+
+gulp.task('previewDocs', function () {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "docs"
+        }
+    });
+});
+
+gulp.task('deleteDocs', function () {
+    return del('./docs');
+});
+gulp.task('optimizeImages', function () {
+    return gulp.src(['./app/assets/img/**/*'])
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true,
+            multipass: true
+        }))
+        .pipe(gulp.dest('./docs/assets/img'));
+});
+
+gulp.task('usemin', ['deleteDocs', 'styles'], function () {
+    return gulp.src('./app/index.html')
+        .pipe(usemin({
+            css: [function () {
+                return rev()
+            }, function () {
+                return cssnano()
+            }]
+        }))
+        .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('build', ['deleteDocs', 'optimizeImages', 'usemin']);
